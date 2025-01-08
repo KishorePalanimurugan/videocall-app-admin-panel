86be494f9b643d88407fd8d9d36a2085e9bcd807
kishorePalanimurugan <kishore@minidreams.in>
2025-01-08 19:04:19 +0530

    config file changed

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import alias from "@rollup/plugin-alias";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    alias({
-      entries: [
-        // Add an alias for the problematic path
-        {
-          find: "../../components/common",
-          replacement: "/src/components/Common",
-        },
-      ],
-    }),
-  ],
-  esbuild: {
-    jsxFactory: "h",
-    jsxFragment: "Fragment",
-  },
+  plugins: [react()],
   server: {
-    host: true, 
+    host: '0.0.0.0',
+    port: 5173,
   },
-  resolve: {
-    alias: {
-      "@common": "/src/components/common",
-    },
-  },
-});
+})
